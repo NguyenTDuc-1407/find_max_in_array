e013e0718e4e9b0ea8c699226a58224e0d4d09f0
Duc.NT <311020ntd@gmail>
2024-08-30 14:53:12 +0700

    update

--- Program.cs
+++ Program.cs
@@ -1,42 +1,63 @@
-﻿int size;
-int[] array;
-do
+﻿string[,] map = {
+            {"*", ".", ".", "."},
+            {".", ".", ".", "."},
+            {".", "*", ".", "."},
+            {".", ".", ".", "."}
+        };
+int MAP_HEIGHT = map.GetLength(0);
+int MAP_WIDTH = map.GetLength(1);
+
+string[,] mapReport = new string[MAP_HEIGHT, MAP_WIDTH];
+for (int yOrdinate = 0; yOrdinate < MAP_HEIGHT; yOrdinate++)
 {
-    Console.WriteLine("Enter a size:");
-    size = Int32.Parse(Console.ReadLine());
-    if (size > 20)
+    for (int xOrdinate = 0; xOrdinate < map.GetLength(0); xOrdinate++)
     {
-        Console.WriteLine("Size should not exceed 20");
-        break;
+        string curentCell = map[yOrdinate, xOrdinate];
+        if (curentCell.Equals("*"))
+        {
+            mapReport[yOrdinate, xOrdinate] = "*";
+        }
+        else
+        {
+            int[,] NEIGHBOURS_ORDINATE = {
+                        {yOrdinate - 1, xOrdinate - 1}, {yOrdinate - 1, xOrdinate}, {yOrdinate - 1, xOrdinate + 1},
+                        {yOrdinate, xOrdinate - 1}, {yOrdinate, xOrdinate + 1},
+                        {yOrdinate + 1, xOrdinate - 1}, {yOrdinate + 1, xOrdinate}, {yOrdinate + 1, xOrdinate + 1},};
 
-    }
-    else
-    {
-        array = new int[size];
-        int i = 0;
-        while (i < array.Length)
-        {
-            Console.WriteLine("Enter element" + (i + 1) + " : ");
-            array[i] = Int32.Parse(Console.ReadLine());
-            i++;
-        }
-        Console.WriteLine("Property list: ");
-        for (int j = 0; j < array.Length; j++)
-        {
-            Console.WriteLine(array[j] + "\t");
-        }
-        int max = array[0];
-        int index = 1;
-        for (int j = 0; j < array.Length; j++)
-        {
-            if (array[j] > max)
+            int minesAround = 0;
+            int length = NEIGHBOURS_ORDINATE.GetLength(0);
+            for (int i = 0; i < length; i++)
             {
-                max = array[j];
-                index = j + 1;
+                int xOrdinateOfNeighbour = NEIGHBOURS_ORDINATE[i, 1];
+                int yOrdinateOfNeighbour = NEIGHBOURS_ORDINATE[i, 0];
+
+                bool isOutOfMapNeighbour = xOrdinateOfNeighbour < 0
+                        || xOrdinateOfNeighbour == MAP_WIDTH
+                        || yOrdinateOfNeighbour < 0
+                        || yOrdinateOfNeighbour == MAP_HEIGHT;
+                if (isOutOfMapNeighbour)
+                {
+                    continue;
+                }
+
+                bool isMineOwnerNeighbour = map[yOrdinateOfNeighbour, xOrdinateOfNeighbour].Equals("*");
+                if (isMineOwnerNeighbour)
+                {
+                    minesAround++;
+                }
             }
+
+            mapReport[yOrdinate, xOrdinate] = minesAround.ToString();
         }
-        Console.WriteLine("The largest property value in the list is " + max + " ,at position " + index);
-
     }
+}
 
-} while (size > 20);
+for (int yOrdinate = 0; yOrdinate < MAP_HEIGHT; yOrdinate++)
+{
+    Console.WriteLine("\n");
+    for (int xOrdinate = 0; xOrdinate < MAP_WIDTH; xOrdinate++)
+    {
+        String currentCellReport = mapReport[yOrdinate, xOrdinate];
+        Console.Write(currentCellReport);
+    }
+}
